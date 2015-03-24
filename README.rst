a549754f3751be9fd71acfaddba9886f6381e284
Takayuk Shimizukawa <shimizukawa@gmail.com>
2015-03-24 18:38:17 +0900

    update document

---
+++ README.rst
@@ -0,0 +1,10 @@
+====================================
+Redshift database backend for Django
+====================================
+
+This product is tested with django-1.7.6 and python-2.7.x
+
+LICENSE
+=======
+Apache Software License
+
--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     url='https://github.com/shimizukawa/django-redshift-backend',
-    license='Apache Software License ',
+    license='Apache Software License',
     author='shimizukawa',
     author_email='shimizukawa@gmail.com',
     description='Redshift database backend for Django',
-    long_description='Redshift database backend for Django is tested with django-1.7.6 and python2.7.x',
+    long_description='Redshift database backend for Django is tested with django-1.7.6 and python-2.7.x',
     install_requires=requires,
 )
 
