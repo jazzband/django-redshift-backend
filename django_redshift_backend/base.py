6a678d0e35e28ab7f0fd39281dd8de82c0a5416f
Takayuk Shimizukawa <shimizukawa@gmail.com>
2015-03-24 18:34:50 +0900

    initial: support 'INSERT INTO' without RETURNING

---
+++ django_redshift_backend/base.py
@@ -0,0 +1,43 @@
+"""
+Redshift database backend for Django based upon django PostgreSQL backend.
+
+Requires psycopg 2: http://initd.org/projects/psycopg2
+"""
+from __future__ import absolute_import
+
+from django.db.backends import (
+    BaseDatabaseValidation,
+)
+from django.db.backends.postgresql_psycopg2.base import (
+    DatabaseFeatures as BasePGDatabaseFeatures,
+    DatabaseWrapper as BasePGDatabaseWrapper,
+    DatabaseOperations as BasePGDatabaseOperations,
+    DatabaseClient,
+    DatabaseCreation,
+    DatabaseIntrospection,
+)
+
+
+class DatabaseFeatures(BasePGDatabaseFeatures):
+    can_return_id_from_insert = False
+
+
+class DatabaseOperations(BasePGDatabaseOperations):
+
+    def last_insert_id(self, cursor, table_name, pk_name):
+        cursor.execute('SELECT MAX({pk}) from {table}'.format(pk=pk_name, table=self.quote_name(table_name)))
+        return cursor.fetchone()[0]
+
+
+class DatabaseWrapper(BasePGDatabaseWrapper):
+    vendor = 'redshift'
+
+    def __init__(self, *args, **kwargs):
+        super(DatabaseWrapper, self).__init__(*args, **kwargs)
+
+        self.features = DatabaseFeatures(self)
+        self.ops = DatabaseOperations(self)
+        self.client = DatabaseClient(self)
+        self.creation = DatabaseCreation(self)
+        self.introspection = DatabaseIntrospection(self)
+        self.validation = BaseDatabaseValidation(self)
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'django',
+    'psycopg2',
+]
+
+setup(
+    name='django-redshift-backend',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://github.com/shimizukawa/django-redshift-backend',
+    license='Apache Software License ',
+    author='shimizukawa',
+    author_email='shimizukawa@gmail.com',
+    description='Redshift database backend for Django',
+    long_description='Redshift database backend for Django is tested with django-1.7.6 and python2.7.x',
+    install_requires=requires,
+)
+
