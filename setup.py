df84d2ee6d486b0076058ecb1bd99f191c0cbd4f
Takayuk Shimizukawa <shimizukawa@gmail.com>
2016-03-07 10:39:15 +0900

    Bump version to 0.3 

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(filename):
 
 setup(
     name='django-redshift-backend',
-    version='0.2.1',
+    version='0.3',
     packages=find_packages(),
     url='https://github.com/shimizukawa/django-redshift-backend',
     license='Apache Software License',
