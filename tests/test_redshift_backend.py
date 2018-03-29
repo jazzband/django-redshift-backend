6127b85dbd67bdd51c74c2c1cd9f841a4f4a97cd
Kosei Kitahara <surgo.jp@gmail.com>
2018-03-29 19:32:21 +0900

    Remove older macros

--- tests/test_redshift_backend.py
+++ tests/test_redshift_backend.py
@@ -2,7 +2,6 @@
 
 import unittest
 
-import django
 from django.db import connections
 from django.core.management.color import no_style
 import pytest
@@ -56,18 +55,11 @@ expected_dml_annotate = norm_sql(
 
 
 class ModelTest(unittest.TestCase):
-
     def check_model_creation(self, model, expected_ddl):
         conn = connections['default']
         statements, params = conn.creation.sql_create_model(model, no_style(), set())
         sql = norm_sql(''.join(statements))
         self.assertEqual(sql, expected_ddl)
-
-    @pytest.mark.skipif(django.VERSION >= (1, 9),
-                        reason="Django-1.9 or later doesn't support sql creation")
-    def test_create_table(self):
-        from testapp.models import TestModel
-        self.check_model_creation(TestModel, expected_ddl_normal)
 
     def test_annotate(self):
         from django.db.models import Count
@@ -79,7 +71,6 @@ class ModelTest(unittest.TestCase):
 
 
 class MigrationTest(unittest.TestCase):
-
     def check_model_creation(self, model, expected_ddl):
         conn = connections['default']
         schema_editor = conn.schema_editor(collect_sql=True)
@@ -88,14 +79,10 @@ class MigrationTest(unittest.TestCase):
         sql = norm_sql(''.join(schema_editor.collected_sql))
         self.assertEqual(sql, expected_ddl)
 
-    @pytest.mark.skipif(django.VERSION < (1, 8),
-                        reason="Django-1.8 or earlier doesn't support migration")
     def test_create_model(self):
         from testapp.models import TestModel
         self.check_model_creation(TestModel, expected_ddl_normal)
 
-    @pytest.mark.skipif(django.VERSION < (1, 8),
-                        reason="Django-1.8 or earlier doesn't support migration")
     def test_create_table_meta_keys(self):
         from testapp.models import TestModelWithMetaKeys
         self.check_model_creation(TestModelWithMetaKeys, expected_ddl_meta_keys)
