7b73d99b1992eecb5f28e6a35e862dce1dd94860
shimizukawa <shimizukawa@gmail.com>
2018-04-11 22:07:18 +0900

    add doc

---
+++ doc/conf.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# -- Project information -----------------------------------------------------
+
+project = 'Django Redshift Backend'
+copyright = '2018, Django Redshift Backend team'
+author = 'Django Redshift Backend team'
+
+version = release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+]
+
+source_suffix = '.rst'
+master_doc = 'index'
+language = None
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+pygments_style = 'sphinx'
+
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'sphinx_rtd_theme'
+# html_theme_options = {}
+
