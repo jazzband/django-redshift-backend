959f2eeb77225f3854beba54354137c9760ed06e
shimizukawa <shimizukawa@gmail.com>
2021-03-08 09:07:48 +0900

    modernize setup.cfg

--- django_redshift_backend/__init__.py
+++ django_redshift_backend/__init__.py
@@ -1,6 +1,14 @@
-from pkg_resources import get_distribution, DistributionNotFound
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    # package is not installed
-    pass
+try:  # py38 or later
+    from importlib.metadata import version, PackageNotFoundError
+    try:
+        __version__ = version("package-name")
+    except PackageNotFoundError:
+        # package is not installed
+        pass
+except ImportError:  # py36, py37
+    from pkg_resources import get_distribution, DistributionNotFound
+    try:
+        __version__ = get_distribution(__name__).version
+    except DistributionNotFound:
+        # package is not installed
+        pass
--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
 from setuptools import setup
 
-setup(
-    setup_requires=['setuptools_scm'],
-    use_scm_version=True,
-)
+setup()
