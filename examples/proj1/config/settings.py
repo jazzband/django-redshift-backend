e81c87d9e92c3d0c89c962efad2207d69f817c93
Takayuki SHIMIZUKAWA <shimizukawa@gmail.com>
2022-02-13 21:27:03 +0000

    gha: add psql test environment

--- examples/proj1/config/settings.py
+++ examples/proj1/config/settings.py
@@ -12,16 +12,21 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import environ
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Take environment variables from .env file
-environ.Env.read_env(BASE_DIR/'.env')
+if env('ENV_FILE', default=None):
+    # Take environment variables from specified file
+    env.read_env(env('ENV_FILE'))
+elif (BASE_DIR/'.env').is_file():
+    # Take environment variables from .env file
+    environ.Env.read_env(BASE_DIR/'.env')
+else:
+    pass  # use environment variable without .env file
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
