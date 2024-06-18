f1404ad6bb1965f2b7dc348c46426888b873eac9
Takayuki SHIMIZUKAWA <shimizukawa@gmail.com>
2024-06-18 12:42:00 +0000

    Remove old code (py37)

--- django_redshift_backend/__init__.py
+++ django_redshift_backend/__init__.py
@@ -1,18 +1,10 @@
-try:  # py38 or later
-    from importlib.metadata import version, PackageNotFoundError
-
-    try:
-        __version__ = version("package-name")
-    except PackageNotFoundError:
-        # package is not installed
-        pass
-except ImportError:  # py37
-    from pkg_resources import get_distribution, DistributionNotFound
-
-    try:
-        __version__ = get_distribution(__name__).version
-    except DistributionNotFound:
-        # package is not installed
-        pass
-
 from django_redshift_backend.meta import DistKey, SortKey  # noqa
+
+# py38 or later
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("package-name")
+except PackageNotFoundError:
+    # package is not installed
+    pass
