343d63cc0e185aed6c6297583e6317f7af04e72f
Takayuki SHIMIZUKAWA <shimizukawa@gmail.com>
2024-08-23 22:10:23 +0000

    updating setup uv

--- .devcontainer/on_create_command.sh
+++ .devcontainer/on_create_command.sh
@@ -1,5 +1,8 @@
 # setup
+set -ex
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
-uvx tool install ruff
-uv sync
+. $HOME/.cargo/env
+uv tool install -U ruff
+uv tool install -U tox --with tox-uv
+uv sync --extra psycopg2-binary
